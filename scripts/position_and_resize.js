34a09a5347fa0e52e312579ae1456e21f88cdc3e
Bo <Bo@lottotech.com>
2015-08-25 10:50:08 +0300

    cross origin test

---
+++ scripts/position_and_resize.js
@@ -0,0 +1,102 @@
+// Resize and position image
+console.log('loaded')
+var resizeableImage = function(image_target) {
+    var $container,
+    orig_src = new Image(),
+    image_target = $(image_target).get(0),
+    event_state = {},
+    constrain = false,
+    min_width = 60,
+    min_height = 60,
+    max_width = 2700,
+    max_height = 2000,
+    resize_canvas = document.createElement('canvas');
+
+    init = function () {
+
+        // Create a new image with a copy of the original src
+        // When resizing, we will always use this original copy as the base
+        orig_src.src = image_target.src;
+
+        // Add resize handles
+        $(image_target).wrap('<div class="resize-container"></div>')
+        .before('<span class="resize-handle resize-handle-nw"></span>')
+        .before('<span class="resize-handle resize-handle-ne"></span>')
+        .after('<span class="resize-handle resize-handle-se"></span>')
+        .after('<span class="resize-handle resize-handle-sw"></span>');
+
+        // Get a variable for the container
+        $container = $(image_target).parent('.resize-container');
+
+        // Add events
+        $container.on('mousedown', '.resize-handle', startResize);
+    };
+
+    saveEventState = function (e) {
+        // Save the initial event details and container state
+        event_state.container_width = $container.width();
+        event_state.container_height = $container.height();
+        event_state.container_left = $container.offset().left;
+        event_state.container_top = $container.offset().top;
+        event_state.mouse_x = (e.clientX || e.pageX || e.originalEvent.touches[0].clientX) + $(window).scrollLeft();
+        event_state.mouse_y = (e.clientY || e.pageY || e.originalEvent.touches[0].clientY) + $(window).scrollTop();
+
+        // This is a fix for mobile safari
+        // For some reason it does not allow a direct copy of the touches property
+        if (typeof e.originalEvent.touches !== 'undefined') {
+            event_state.touches = [];
+            $.each(e.originalEvent.touches, function (i, ob) {
+                event_state.touches[i] = {};
+                event_state.touches[i].clientX = 0 + ob.clientX;
+                event_state.touches[i].clientY = 0 + ob.clientY;
+            });
+        }
+        event_state.evnt = e;
+    }
+
+    startResize = function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        saveEventState(e);
+        $(document).on('mousemove', resizing);
+        $(document).on('mouseup', endResize);
+    };
+
+    endResize = function (e) {
+        e.preventDefault();
+        $(document).off('mouseup touchend', endResize);
+        $(document).off('mousemove touchmove', resizing);
+    };
+
+    resizing = function (e) {
+        var mouse = {}, width, height, left, top, offset = $container.offset();
+        mouse.x = (e.clientX || e.pageX || e.originalEvent.touches[0].clientX) + $(window).scrollLeft();
+        mouse.y = (e.clientY || e.pageY || e.originalEvent.touches[0].clientY) + $(window).scrollTop();
+
+        width = mouse.x - event_state.container_left;
+        height = mouse.y - event_state.container_top;
+        left = event_state.container_left;
+        top = event_state.container_top;
+
+        if (constrain || e.shiftKey) {
+            height = width / orig_src.width * orig_src.height;
+        }
+
+        if (width > min_width && height > min_height && width < max_width && height < max_height) {
+            resizeImage(width, height);
+            // Without this Firefox will not re-calculate the the image dimensions until drag end
+            $container.offset({ 'left': left, 'top': top });
+        }
+    }
+    resizeImage = function (width, height) {
+        resize_canvas.width = width;
+        resize_canvas.height = height;
+        resize_canvas.getContext('2d').drawImage(orig_src, 0, 0, width, height);
+        $(image_target).attr('src', resize_canvas.toDataURL("image/png"));
+    };
+
+    init();
+};
+
+resizeableImage($('.resize-image'));
+
