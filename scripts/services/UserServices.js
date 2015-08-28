ab84a97d7a1de37d5c2a7ebc2b989e784e580a58
tcholakov <tcholakov2@gmail.com>
2015-08-29 01:40:14 +0300

    Added unversioned files

---
+++ scripts/services/UserServices.js
@@ -0,0 +1,14 @@
+var DBOperations = require('../DBOperations.js');
+
+var UserServices = (function () {
+    var userServices;
+    
+    userServices = {
+        createUser: function(username, password){
+        }
+    }
+
+    return userServices;
+})();
+
+module.exports = UserServices;
