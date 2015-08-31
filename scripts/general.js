73a83a62ade8ccf4ad609b3c7b3e25666d372a26
Bo <Bo@lottotech.com>
2015-08-31 08:25:33 +0300

    added switch for login and register forms

--- scripts/general.js
+++ scripts/general.js
@@ -4,8 +4,16 @@ var imageIdCounter = 0;
 
 var images = [];
 
+$('#switch_to_register').on('click', function () {
+    console.log('clicked!')
+    $('.slideable').addClass('slided');
+});
+$('#switch_to_login').on('click', function () {
+    $('.slideable').removeClass('slided');
+});
+
 ////// fake functionality for the login
-$(document).on('click', $('.login'), function () {
+$('.welcome_holder h2').on('click', function () {
     $('#welcome_screen').hide();
 });
 
