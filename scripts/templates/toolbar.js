cf08ac39bd2966ae0bc4d9c94cb8bfe17a01c726
Bo <Bo@lottotech.com>
2015-09-02 07:46:03 +0300

    rearranging buttons

--- scripts/templates/toolbar.js
+++ scripts/templates/toolbar.js
@@ -6,11 +6,11 @@ function drawToolbar() {
         '<button id="load_photo" class="btn btn-default" title="Load">Load</button>' +
         '<button id="take_snapshot" class="btn btn-default" title="Take photo">Photo</button>' +
         '<button id="reset" class="btn btn-default" title="Reset">Reset</button>' +
-        '<button id="crop_image" class="btn btn-default" title="Crop">Crop</button>' +
+       // '<button id="crop_image" class="btn btn-default" title="Crop">Crop</button>' +
         '<button id="save_image_button" class="btn btn-default" title="Save">Save</button>' +
-        '<a href="https://www.facebook.com/sharer/sharer.php?u=example.org" target="_blank">' +
-        '<button id="share_picture" title="Share" class="btn btn-default">Share</button>' +
-        '</a>';
+       // '<a href="https://www.facebook.com/sharer/sharer.php?u=example.org" target="_blank">' +
+        '<button id="share_picture" title="Share" class="btn btn-default">Share</button>';// +
+       // '</a>';
 
 
         var tableTemplate = Handlebars.compile(template);
